2942af66db4e8d2f24b0e69f5da47e5038b520b4
Nikhileshlingamgunta <124075195+Nikhileshlingamgunta@users.noreply.github.com>
2023-11-09 10:07:05 -0500

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import Dashboard from "./Dashboard";
+import { CartState } from '../Context';
+import { useEffect, useRef, useState } from 'react';
+
+import SingleProduct from "./SingleProduct";
+
 
 function Home(){
+    const {
+        state: { products },
+        //productState: { sort, byStock, byFastDelivery, byRating, searchQuery },
+      } = CartState();
+    
+      React.useEffect(() => {
+        console.log("data from home",products);
+      }, [products]);
+
     return(
-<div>
-    <Dashboard />
-</div>
+        <div>
+            <Dashboard />
+            <div className="home">
+            {/* <Filters /> */}
+                <div className="productContainer">
+                    {products.map((prod) => (
+                    <SingleProduct prod={prod} key={prod.id} />
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
