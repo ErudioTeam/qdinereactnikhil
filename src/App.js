aedc0d40689b039fa6c1f9edabd097817c2d1d86
Nikhileshlingamgunta <124075195+Nikhileshlingamgunta@users.noreply.github.com>
2023-11-09 09:46:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
 import Users from './Components/Users';
@@ -6,6 +7,9 @@ import Login from './Components/Login';
 import Register from './Components/Register'
 import Dashboard from './Components/Dashboard';
 import Home from './Components/Home';
+import Cart from './Components/Cart';
+import OwnerMenu from './Components/OwnerMenu';
+import ChefMenu from './Components/ChefMenu';
 
 
 function App() {
@@ -19,6 +23,9 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/OwnerMenu" element={<OwnerMenu />} />
+          <Route path="/ChefMenu" element={<ChefMenu />} />
         </Routes>
       </BrowserRouter>
     </div>
